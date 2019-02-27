cd3a266646db13bfdd33a2c29b918d9ca8e5b362
Lijo Jose <lijo@etherlabs.io>
2019-02-27 13:37:41 +0530

    initial version

---
+++ cpusvc/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	flag1 := true
+	sleeptime := 1000
+	count := 0
+	for flag1 {
+		fmt.Printf("count %d sleeptime %d\n", count, sleeptime)
+		count++
+		time.Sleep(time.Duration(sleeptime) * time.Millisecond)
+		dat, err := ioutil.ReadFile("./dat.txt")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		datStr := string(dat)
+		if strings.Contains(datStr, "s=") {
+			sleeptime, err = strconv.Atoi(strings.Split(datStr, "=")[1])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+		if strings.Contains(datStr, "e=") {
+			fmt.Println("exiting as flag is set to false")
+			flag1 = false
+		}
+	}
+}
